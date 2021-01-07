e0f6a342ffd8d6fd28abae3f8d2891d270385ddb
Nakoshima <64407502+Nakoshima@users.noreply.github.com>
2021-01-07 14:39:28 +0100

    Update nqueen_solving.py
    minor modifications in order to respect flake8

--- nqueen_solving.py
+++ nqueen_solving.py
@@ -5,6 +5,7 @@ import copy
 import random
 import nqueen_solving_util as util
 
+
 def print_board(size, board):
     """
     Prints the chessboard
@@ -12,11 +13,12 @@ def print_board(size, board):
     :param board: the chessboard
     """
     board_display = ''
-    for line in range(size):
+    for row in range(size):
         for column in range(size):
-            board_display += (' ' + str(board[line][column]))
+            board_display += (' ' + str(board[row][column]))
         board_display += '\n'
-    print(board_display, end = '')
+    print(board_display, end='')
+
 
 def can_t_attack(size, board):
     """
@@ -27,16 +29,20 @@ def can_t_attack(size, board):
     """
     return util.can_t_attack(size, board)
 
+
 def is_soluce(size, board):
     """
     Checks if a board is a solution to the n queens problem
     :param size: the size of the chessboard
     :param board: the chessboard
-    :returns: True if the board is a solution, False if not and the number queens
+    :returns:
+        True if the board is a solution, False if not
+        The number queens
     """
     # Counts the number of queens on the board
     nb_queen = sum([i.count(1) for i in board])
     return util.can_t_attack(size, board) and nb_queen == size, nb_queen
+
 
 def solve_n_queen_small(size, board):
     """
@@ -44,7 +50,9 @@ def solve_n_queen_small(size, board):
     a backtracking algorithm
     :param size: the size of the chessboard
     :param board: the chessboard
-    :returns: the board and True if a solution is found, False if not
+    :returns:
+        The board
+        True if a solution is found, False if not
     """
     if size == 1:
         return [1], True
@@ -53,9 +61,10 @@ def solve_n_queen_small(size, board):
         return board, False
 
     # We start placing the queens from the bottom right
-    line = size - 1
+    row = size - 1
 
-    return util.backtrack_n_queen(size, board, line), True
+    return util.backtrack_n_queen(size, board, row), True
+
 
 def solve_n_queen_all_soluce(size, board):
     """
@@ -73,6 +82,7 @@ def solve_n_queen_all_soluce(size, board):
 
     return util.backtrack_all_soluce(size, board)
 
+
 def solve_n_queen_big(size, board):
     """
     Solves big sized n queens problems by using
@@ -80,23 +90,28 @@ def solve_n_queen_big(size, board):
     Reasonable resolution time up to size = 200 (~1 min 30 s)
     :param size: the size of the chessboard
     :param board: the chessboard
-    :returns: the board and True if a solution is found, False if not
+    :returns:
+        The board
+        True if a solution is found, False if not
     """
-    current_queens = util.initial_state(size)
+    queens = util.initial_state(size)
 
-    current_heuristic = util.calculate_heuristic(current_queens)
+    current_heuristic = util.calculate_heuristic(queens)
 
     while current_heuristic != 0:
         # Chooses a random child board
-        new_queens = copy.deepcopy(current_queens)
-        index1, index2 = random.randint(0, size - 1), random.randint(0, size - 1)
+        new_queens = copy.deepcopy(queens)
+
+        i = random.randint(0, size - 1)
+        j = random.randint(0, size - 1)
 
         # Temporarily swaps the queens if one of them is attacked
-        if util.is_attacked(current_queens, index1) or util.is_attacked(current_queens, index2):
-            new_queens[index1], new_queens[index2] = new_queens[index2], new_queens[index1]
-            # Definitely swaps the queens if the new heuristic is lower than the current one
+        if util.is_attacked(queens, i) or util.is_attacked(queens, j):
+            new_queens[i], new_queens[j] = new_queens[j], new_queens[i]
+            # If the new heuristic is lower than the current one
+            # definitely swaps the queens
             if util.calculate_heuristic(new_queens) <= current_heuristic:
-                current_queens = new_queens
-        current_heuristic = util.calculate_heuristic(current_queens)
-    board = util.fill_board(current_queens)
+                queens = new_queens
+        current_heuristic = util.calculate_heuristic(queens)
+    board = util.fill_board(queens)
     return board, True
